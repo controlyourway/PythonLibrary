d2ce8df12190ee1f4524cf72d8ff9d118fd19a1e
Hubert Jetschko <hubert@robospace.co.nz>
2017-11-14 18:17:37 +1300

    + Add and move files so package can be installed on PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ControlYourWay',
+  packages = ['ControlYourWay'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Library used to send and receive data using the Control Your Way service',
+  author = 'Hubert Jetschko',
+  author_email = 'hubert@controlyourway.com',
+  url = 'https://github.com/controlyourway/PythonLibrary', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
